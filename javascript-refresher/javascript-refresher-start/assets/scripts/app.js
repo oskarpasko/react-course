b12afce4ddf7763968924d82f1ec4ef03a877e4a
Oskar <oskarpasko@gmail.com>
2023-10-12 14:28:18 +0200

    import and export in js

---
+++ javascript-refresher/javascript-refresher-start/assets/scripts/app.js
@@ -0,0 +1,10 @@
+// import { apiKey } from "./util";
+
+// import apiKey from "./util";
+
+import * as utils from "./util";
+
+console.log(utils);
+console.log(utils.default);
+console.log(utils.apiKey);
+console.log(utils.abc);
---
+++ javascript-refresher/javascript-refresher-start/assets/scripts/util.js
@@ -0,0 +1,4 @@
+// export let  apiKey = "nfadsjknfajks";
+export default "default export";
+export let  apiKey = "nfadsjknfajks";
+export let abc = "cos nnowego";
