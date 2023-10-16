393100297029deb66937369241e2a10a0b5a49d1
Oskar <oskarpasko@gmail.com>
2023-10-16 15:23:22 +0200

    change some code in component to output a date

--- 03-react-basics-and-working-with-components/react-basics/src/App.js
+++ 03-react-basics-and-working-with-components/react-basics/src/App.js
@@ -31,11 +31,6 @@ function App() {
         date={expenses[0].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
